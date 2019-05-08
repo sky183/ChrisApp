3428a30cd892c20f43ef5737e75e3f4e0b4be119
ChrisApp <sky183>
2019-05-08 16:14:17 +0900

    test8

--- app/src/main/java/com/example/chrisapp/MainActivity.java
+++ app/src/main/java/com/example/chrisapp/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.chrisapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button button;
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("버튼1");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button2.setText("버튼1");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button3.setText("버튼1");
+            }
+        });
+
     }
 }
