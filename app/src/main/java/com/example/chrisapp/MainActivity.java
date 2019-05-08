8b4cb13db4dcb441f5c28417db7a4bf42aee2755
nuriapp <nuriapp>
2019-05-09 00:53:38 +0900

    test11

--- app/src/main/java/com/example/chrisapp/MainActivity.java
+++ app/src/main/java/com/example/chrisapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.chrisapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +13,32 @@ public class MainActivity extends Activity {
     Button button2;
     Button button3;
 
-    private BackPressCloseHandler backPressCloseHandler;
+//    private BackPressCloseHandler backPressCloseHandler;
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+                /*backPressCloseHandler.onBackPressed();*/ //뒤로 두번 누르면 종료하기 기능
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("아날로그 엔진 종료");
+        builder.setMessage("아날로그 엔진을 종료하시겠습니까?")
+                .setCancelable(false)
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +47,7 @@ public class MainActivity extends Activity {
         button = (Button) findViewById(R.id.button);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
-        backPressCloseHandler = new BackPressCloseHandler(this);
+//        backPressCloseHandler = new BackPressCloseHandler(this);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,11 +75,6 @@ public class MainActivity extends Activity {
                 button2.setBackgroundResource(R.drawable.buttonshape2);
             }
         });
-
-        @Override public void onBackPressed ()
-        {
-            backPressCloseHandler.onBackPressed();
-        }
 
 
     }
