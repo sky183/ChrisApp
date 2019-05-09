5988b29d5e5e7a89e8a20080624efb56aca94b23
ChrisApp <sky183>
2019-05-09 13:27:41 +0900

    test12

--- app/src/main/java/com/example/chrisapp/MainActivity.java
+++ app/src/main/java/com/example/chrisapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chrisapp;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,29 +20,29 @@ public class MainActivity extends Activity {
     public void onBackPressed() {
 //        super.onBackPressed();
                 /*backPressCloseHandler.onBackPressed();*/ //뒤로 두번 누르면 종료하기 기능
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("아날로그 엔진 종료");
-        builder.setMessage("아날로그 엔진을 종료하시겠습니까?")
-                .setCancelable(false)
-                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this); //다이얼로그 내부클래스 빌더 객체 builder 생성
+        builder.setTitle("아날로그 엔진 종료"); //다이얼로그의 제목
+        builder.setMessage("아날로그 엔진을 종료하시겠습니까?")  //다이얼로그의 내용
+                .setCancelable(false)  //뒤로가기나 배경 터치로 다이얼로그를 취소하는 행위를 못하게 함
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() { //확인 버튼 이벤트
                     @Override
                     public void onClick(DialogInterface dialog, int i) {
-                        finish();
+                        finish(); //페이지 종료
                     }
                 })
-                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() { //캔슬 버튼 이벤트
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
+                        dialog.cancel(); //다이얼로그를 끈다.
                     }
                 });
-        AlertDialog alert = builder.create();
-        alert.show();
+        AlertDialog alert = builder.create(); //빌더 객체의 create 메서드로 다이얼로그 객체를 생성한다.
+        alert.show(); //다이얼로그를 보여준다.
 
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
@@ -49,7 +50,25 @@ public class MainActivity extends Activity {
         button3 = (Button) findViewById(R.id.button3);
 //        backPressCloseHandler = new BackPressCloseHandler(this);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(intent); //다음화면으로 넘어감
+                overridePendingTransition(0,0);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(intent); //다음화면으로 넘어감
+                overridePendingTransition(0,0);
+            }
+        });
+
+       /* button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 button.setBackgroundResource(R.drawable.click1);
@@ -74,7 +93,7 @@ public class MainActivity extends Activity {
                 button.setBackgroundResource(R.drawable.buttonshape);
                 button2.setBackgroundResource(R.drawable.buttonshape2);
             }
-        });
+        });*/
 
 
     }
---
+++ app/src/main/java/com/example/chrisapp/MainActivity2.java
@@ -0,0 +1,50 @@
+package com.example.chrisapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends Activity {
+
+    Button button;
+    Button button2;
+    Button button3;
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(0,0);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent); //다음화면으로 넘어감
+                overridePendingTransition(0,0);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(intent); //다음화면으로 넘어감
+                overridePendingTransition(0,0);
+            }
+        });
+
+
+    }
+}
