5a69cc488bfc869282c1c4e65fa69aa874d1decb
nuriapp <nuriapp>
2019-05-13 01:57:12 +0900

    test17

--- app/src/main/java/com/example/chrisapp/DetailActivity1.java
+++ app/src/main/java/com/example/chrisapp/DetailActivity1.java
@@ -32,6 +32,7 @@ public class DetailActivity1 extends Activity {
         StrictMode.setThreadPolicy(policy);
 
         TextView tvTitle = (TextView)findViewById(R.id.textView1);
+        TextView tvTitle2 = (TextView)findViewById(R.id.textView2);
         ImageView iv = (ImageView)findViewById(R.id.imageView1);
 
         Intent intent = getIntent(); // 보내온 Intent를 얻는다
@@ -50,6 +51,7 @@ public class DetailActivity1 extends Activity {
         }
 
         tvTitle.setText(intent.getStringExtra("title"));
+        tvTitle2.setText(intent.getStringExtra("title2"));
 
     } // end of onCreate
 }
