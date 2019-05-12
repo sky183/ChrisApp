1ca6852e45e5ca95f55b7214cac250db0cb8ac26
nuriapp <nuriapp>
2019-05-13 01:51:35 +0900

    test17

---
+++ app/src/main/java/com/example/chrisapp/DetailActivity3.java
@@ -0,0 +1,55 @@
+package com.example.chrisapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class DetailActivity3 extends Activity {
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(0,0);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity3detail);
+
+        //URL로 이미지 가져오기 위해 필수!!
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        TextView tvTitle = (TextView)findViewById(R.id.textView1);
+        ImageView iv = (ImageView)findViewById(R.id.imageView1);
+
+        Intent intent = getIntent(); // 보내온 Intent를 얻는다
+//        iv.setImageResource(intent.getIntExtra("img", 0));
+
+        try {
+            URL url = new
+                    URL(intent.getStringExtra("img"));
+            URLConnection conn = url.openConnection();
+            conn.connect();
+            InputStream stream = conn.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(stream);
+            stream.close();
+            iv.setImageBitmap(bitmap);
+        } catch (Exception e) {
+        }
+
+        tvTitle.setText(intent.getStringExtra("title"));
+
+    } // end of onCreate
+}
--- app/src/main/java/com/example/chrisapp/MainActivity.java
+++ app/src/main/java/com/example/chrisapp/MainActivity.java
@@ -37,17 +37,17 @@ public class MainActivity extends Activity {
         //문자열 데이터를 저장하는데 사용할 ArrayList 객체 생성
         List<String[]> list = new ArrayList<>();
 
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1452_s.png", "MILEAGE PLATINUM"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1248.png", "삼성카드 4"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1483_s.png", "삼성카드 taptap O"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1151_s.png", "American Express® Green"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1482_s.png", "삼성카드 taptap S"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1562_s.png", "삼성카드 2 V3"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1567_s.png", "삼성카드 5 V3"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1602_s.png", "트레이더스신세계 삼성카드"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1565_s.png", "삼성카드 4 V3"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1563_s.png", "삼성카드 3 V3 (SK에너지)"});
-        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1521_s.png", "삼성 페이 삼성카드 taptap"});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1452_s.png", "MILEAGE PLATINUM", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1248.png", "삼성카드 4", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1483_s.png", "삼성카드 taptap O", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1151_s.png", "American Express® Green", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1482_s.png", "삼성카드 taptap S", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1562_s.png", "삼성카드 2 V3", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1567_s.png", "삼성카드 5 V3", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1602_s.png", "트레이더스신세계 삼성카드", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1565_s.png", "삼성카드 4 V3", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1563_s.png", "삼성카드 3 V3 (SK에너지)", "멋진카드입니다."});
+        list.add(new String[]{"http://192.168.35.115/MyAppServer/photo1/AAP1521_s.png", "삼성 페이 삼성카드 taptap", "멋진카드입니다."});
 
         //리스트뷰에 연결하기 위한 어댑터 생성자
         ArrayAdapter adapter = new ImageAdapter(this, list);
@@ -64,6 +64,7 @@ public class MainActivity extends Activity {
                 String[] text = (String[]) parent.getItemAtPosition(position);
                 String imgText = text[0];
                 String strText = text[1];
+                String contentText = text[2];
 
                 // strText 를 사용하여 토스트 알림을 띄운다.
                 Toast.makeText(MainActivity.this, strText, Toast.LENGTH_SHORT).show();
@@ -80,6 +81,7 @@ public class MainActivity extends Activity {
                 // 리스트뷰 클릭시 인텐트 (Intent) 생성하고 position 값을 이용하여 인텐트로 넘길값들을 넘긴다
                 intent.putExtra("img", imgText);
                 intent.putExtra("title", strText);
+                intent.putExtra("content", contentText);
 
                 startActivity(intent); //다음화면으로 넘어감
 
